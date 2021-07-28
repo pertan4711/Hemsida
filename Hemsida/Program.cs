55a45df65141625db7e41b4d07f1273c48ce2ae1
Per Stensland <per.stensland@avensia.com>
2021-07-28 09:36:40 +0200

    Uppgraderat till .NET Core 3.1

--- Hemsida/Program.cs
+++ Hemsida/Program.cs
@@ -1,14 +1,11 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Hemsida.Data;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-
 
 namespace Hemsida
 {
@@ -16,35 +13,14 @@ namespace Hemsida
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            RunSeeding(host);
-            host.Run();
-        }
-
-        private static void RunSeeding(IHost host)
-        {
-            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
-            using (var scope = scopeFactory.CreateScope())
-            {
-                var seeder = scope.ServiceProvider.GetService<HemsidaSeeder>();
-                seeder.SeedAsync().Wait();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration(SetupConfiguration)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
-
-        private static void SetupConfiguration(HostBuilderContext ctx, IConfigurationBuilder builder)
-        {
-            builder.Sources.Clear();
-
-            builder.AddJsonFile("config.json", false, true)
-                .AddEnvironmentVariables();
-        }
     }
 }
--- Hemsida/Startup.cs
+++ Hemsida/Startup.cs
@@ -1,55 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using AutoMapper;
-using Hemsida.Data;
-using Hemsida.Data.Entities;
-using Hemsida.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Hemsida
 {
     public class Startup
     {
-        private readonly IConfiguration _config;
-
-        public Startup(IConfiguration config)
+        public Startup(IConfiguration configuration)
         {
-            _config = config;
+            Configuration = configuration;
         }
 
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentity<StoreUser, IdentityRole>(cfg =>
-                {
-                    cfg.User.RequireUniqueEmail = true;
-                })
-                .AddEntityFrameworkStores<HemsidaContext>();
-
-            services.AddDbContext<HemsidaContext>(cfg =>
-            {
-                cfg.UseSqlServer(_config.GetConnectionString("HemsidaConnectionString"));
-            });
-
-            services.AddTransient<HemsidaSeeder>();
-            services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            services.AddScoped<IHemsidaRepository, HemsidaRepository>();
-            services.AddTransient<IMailService, NullMailService>();
-
-            services.AddControllersWithViews()
-                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,23 +35,23 @@ namespace Hemsida
             }
             else
             {
-                app.UseExceptionHandler("/error");
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
-
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseNodeModules();
-
-            app.UseAuthentication();
 
             app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(cfg =>
-            {
-                cfg.MapControllerRoute("Default",
-                      "{controller}/{action}/{id?}",
-                      new { controller = "App", Action = "Index" });
-            });
 
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
