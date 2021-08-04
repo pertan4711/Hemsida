7cb5517cecd081f8a7af391ff18bc9b3c5e22442
Per Stensland <per.stensland@avensia.com>
2021-08-04 16:32:11 +0200

    adding missing database migration

--- Hemsida/Program.cs
+++ Hemsida/Program.cs
@@ -1,5 +1,8 @@
+using Hemsida.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,20 @@ namespace Hemsida
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            MigrateDatabase(host);
+
+            host.Run();
+        }
+
+        private static void MigrateDatabase(IHost host)
+        {
+            using(var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<HemsidaDbContext>();
+                db.Database.Migrate();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
