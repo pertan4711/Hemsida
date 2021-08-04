011050053132e0bae6f1a8c0f53a354532f7d805
Per Stensland <per.stensland@avensia.com>
2021-08-04 13:55:58 +0200

    dotnet5

--- Hemsida/Startup.cs
+++ Hemsida/Startup.cs
@@ -34,6 +34,7 @@ namespace Hemsida
             });
 
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
+            //services.AddScoped<IRestaurantData, InMemoryRestaurantData>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
